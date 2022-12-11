923b30499fc021efa2bed94c652f03853730bf79
Mariana Daruix <mariana.daruix@gmail.com>
2022-12-10 22:25:54 -0300

    adiciona exercicio 7 semana 2 modulo 1

---
+++ Ex07/index.js
@@ -0,0 +1,20 @@
+//Percorra os números de o a 1000 e exiba no console os números primos 
+//e ao final a quantidade de números primos que existem nesse intervalo.
+//Realize o commit do exercício resolvido.
+
+var contador = 0;
+
+for(var i = 2; i <= 1000; i++){
+    var primo = true;
+    for(var j = 2; j < i; j++){
+        if(i % j === 0){
+            primo = false;
+        }
+    }
+    if(primo){
+        contador += 1;
+        console.log(i);
+    }
+}
+
+console.log('Quantidade: ', contador);
